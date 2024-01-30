bd86bc65281884e8bceda32458e95145d87dc998
sulavadhikari <cabbage.adhikari18@gmail.com>
2024-01-31 03:17:23 +0545

    user registration view

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            
+            token = user.token
+
+            return Response({'user_id': user.id, 'token': token}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
