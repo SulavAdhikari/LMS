06417e29a079c64cf05f8d71ca84968e102aea54
SulavAdhikari <cabbage.adhikari18@gmail.com>
2024-01-30 21:51:14 +0545

    Changed User Model

--- UserApp/models.py
+++ UserApp/models.py
@@ -1,13 +1,16 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
-    username = None
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
