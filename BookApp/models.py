9f5f807a22dc75b728452bd203c089bbca7dc180
SulavAdhikari <cabbage.adhikari18@gmail.com>
2024-01-30 22:28:52 +0545

    models for book

--- BookApp/models.py
+++ BookApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import get_user_model()
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    Published_date = models.DateField(auto_now=True)
+    
+class BookDetail(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    NumberOfPages = models.IntegerField()
+    language = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    
+class BorrowedBook(models.Model):
+    user = models.ForeignKey(get_user_modedl(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date =  models.DateField()
+    return_date =  models.DateField(null=True)
