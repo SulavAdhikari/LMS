1fa59997606f7b50defdaba4ddbd106fb60d8348
sulavadhikari <cabbage.adhikari18@gmail.com>
2024-01-31 05:19:30 +0545

    serializer modified

--- UserApp/serializers.py
+++ UserApp/serializers.py
@@ -9,7 +9,6 @@ class UserCreateSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
-            username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
